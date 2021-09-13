886848d0173a2916416775f87dd06a283205db25
rizkidarmawan21 <rizkidarmawan.0402102@gmail.com>
2021-09-13 22:41:28 +0700

    first commit

---
+++ app/src/main/java/com/rizkidarms/hellotoats/MainActivity.java
@@ -0,0 +1,40 @@
+package com.rizkidarms.hellotoats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private int mCount = 0;
+    private TextView mShowCount;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView)
+                findViewById(R.id.show_count);
+    }
+
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+
+
+    }
+    public void countDown(View view) {
+        mCount--;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+
+
+    }
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, "Hallo Rizki ", Toast.LENGTH_LONG);
+        toast.show();
+    }
+}
